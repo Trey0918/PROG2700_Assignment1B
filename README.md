a5324409494a50b88d36c97b0efc0dc2c2b02c65
Trey <raptors09185@gmail.com>
2024-03-17 13:03:17 -0300

    added wordmap into the function

---
+++ README.md
@@ -0,0 +1,21 @@
+<img width="150px" src="https://www.nscc.ca/img/aboutnscc/visual-identity-guidelines/artwork/nscc-jpeg.jpg" >
+
+# PROG 2700 - Assignment 1B
+
+### PhoneWords
+
+Create a JavaScript application that will convert a *PhoneWord* or Vanity Number (https://en.wikipedia.org/wiki/Phoneword) to a set of numbers. An example would be taking 1-800-HOLIDAY and getting 18004654329. 
+
+The image below shows the keypad you will be emulating:
+
+<img width="400px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Telephone-keypad2.svg/2560px-Telephone-keypad2.svg.png" alt="dialpad">
+
+**Note:** in North America, the alphabet begins with the number 2 on the keypad. You have been provided with starter code in this repository (index.html and main.js). The code that you are asked to write should go in the function indicated called *convertPhoneWord*. The script will execute a *testCode()* function that will test your code.  Errors will be written to the console until your code is sufficient implemented. **Do not write any code outside of the area indicated.**
+
+#### Requirements
+Given any PhoneWord:
++ Ignore any non -alphanumeric characters: (), -, etc.
++ Keep any existing digits
++ Resolve a letter to a number according to the keypad image
++ Complete the *convertPhoneWord* function such that all tests pass.
+
---
+++ main.js
@@ -0,0 +1,63 @@
+/*
+* PHONEWORDS
+* Write a function that will take a phone word (vanity number) and return the correct telephone number.
+* The number pad looks like the following:
+* https://en.wikipedia.org/wiki/Telephone_keypad#/media/File:Telephone-keypad2.svg
+*
+* RULES
+* Given a phoneword:
+* 1. Ignore any non-alphanumeric characters (), -, etc.
+* 2. Keep any existing digits
+* 3. Resolve a letter to a number according to the keypad image
+* 4. All your code must be contained in the section outlined below
+*/
+
+
+var buttons = ["abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"] //DO NOT MODIFY
+
+
+// YOU CAN ADD TO AND MODIFY THE CODE BELOW THIS LINE
+//   |
+//   V
+
+function convertPhoneWord(phoneWord) {
+
+    //Enter your code in this function body
+    const letterMap = {
+        'A': '2', 'B': '2', 'C': '2',
+        'D': '3', 'E': '3', 'F': '3',
+        'G': '4', 'H': '4', 'I': '4',
+        'J': '5', 'K': '5', 'L': '5',
+        'M': '6', 'N': '6', 'O': '6',
+        'P': '7', 'Q': '7', 'R': '7', 'S': '7',
+        'T': '8', 'U': '8', 'V': '8',
+        'W': '9', 'X': '9', 'Y': '9', 'Z': '9',
+    }
+  
+
+}
+
+//    ^
+//    |
+//YOU CAN ADD TO OR MODIFY THE CODE ABOVE THIS LINE
+
+
+
+
+// DO NOT CHANGE ANY CODE AFTER THIS LINE.
+//     |
+//     |
+//     V
+
+//helper functions...do not modify, but you can use them in your code
+
+function isDigit(character) {
+    return "0123456789".indexOf(character) !== -1;
+}
+
+function isLetter(character) {
+    character = character.toUpperCase();    
+    return "ABCDEFGHIJKLMNOPQRSTUVWXYZ".indexOf(character) !== -1;
+}
+
+testCode();
